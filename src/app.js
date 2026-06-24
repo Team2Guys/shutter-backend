deb4fcb6aa4960addad5cff3440a0cc8224657d0
AhmedSaddique <ahmed.saddique12@gmail.com>
2026-06-24 22:29:24 +0500

    crash issue

--- src/app.js
+++ src/app.js
@@ -4,9 +4,6 @@ import { logger, getServerInfo } from "#lib/index.js";
 
 const { PORT } = env;
 
-// Local / long-running process entry point (`npm start`, `npm run dev`).
-// On Vercel the app is served via the serverless handler in `api/index.js`
-// and this file is never executed.
 init()
   .then(() => app.listen(PORT, () => logger.info(getServerInfo())))
   .catch((error) => {
